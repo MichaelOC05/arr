35178c1b18e0e04e60460cf6cd74a40929022563
Gio <giomedina321@live.com>
2022-08-23 12:55:54 -0700

    team worked together to add models

--- monolith/monolith_rest/models.py
+++ monolith/monolith_rest/models.py
@@ -1,3 +1,60 @@
+from turtle import ondrag
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+
+
+class UserModel(models.Model):
+    user = models.CharField(max_length=20)  
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    
+    
+
+class ReviewModel(models.Model):
+    movie_name = models.CharField(max_length = 100)
+    base_rating = models.SmallIntegerField(validators=[MaxValueValidator(10),MinValueValidator(1)])
+    plot_rating = models.SmallIntegerField(validators=[MaxValueValidator(10),MinValueValidator(1)])# rating based on plot
+    char_rating = models.SmallIntegerField(validators=[MaxValueValidator(10),MinValueValidator(1)])# rating based on character accuracy 
+    setting_rating = models.SmallIntegerField(validators=[MaxValueValidator(10),MinValueValidator(1)])# rating based on setting accuracy 
+    add_on_rating = models.SmallIntegerField(validators=[MaxValueValidator(10),MinValueValidator(1)])# rating based on if they added anything that wasnt in source material 
+    removal_rating = models.SmallIntegerField(validators=[MaxValueValidator(10),MinValueValidator(1)])# rating based on if they took anything out that was in the source material 
+    rubric_rating = models.FloatField()
+    admin_rating = models.FloatField()
+    rating_description = models.TextField(max_length= 250)
+    reviewer_id = models.ForeignKey(
+        UserModel,
+        related_name = "review_model",
+        on_delete=models.PROTECT,  
+        null=False,
+    )
+
+
+class CommentsModel(models.Model):
+    date_posted = models.DateTimeField()
+    comment = models.TextField(max_length=250)
+    commenter_id = models.ForeignKey(
+        UserModel,
+        related_name = "comments_model",
+        on_delete=models.PROTECT,  
+        null=False,
+    )
+
+class MovieInformationModel(models.Model):
+    movie_name = models.CharField(max_length= 100)
+    movie_poster = models.URLField(null=True)
+    source_cover = models.URLField(null=True)
+    movie_director = models.CharField(max_length=100)
+    source_author = models.CharField(max_length=100)
+    imdb_score = models.FloatField()
+    movie_synopsis = models.TextField(max_length=250)
+    imdb_id = models.CharField(max_length=100)
+    source_type = models.CharField(max_length=100)
+    list_of_reviews = models.ForeignKey(
+        ReviewModel,
+        related_name="list_of_reviews",
+        on_delete=models.PROTECT,
+    
+    )
