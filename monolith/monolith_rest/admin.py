eb615ad4f50bd05944a7c9f5efa04da623d1e7a7
Kurt <kurtgreen85@gmail.com>
2022-08-24 19:17:38 -0500

    finished views

--- monolith/monolith_rest/admin.py
+++ monolith/monolith_rest/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-
+from monolith_rest.models import ReviewModel, CommentsModel, MovieInformationModel
 # Register your models here.
-from .models import UserModel
+# from .models import USER_MODEL
 
-@admin.register(UserModel)
-class UserModelAdmin(admin.ModelAdmin):
+# @admin.register(USER_MODEL)
+# class USER_MODELAdmin(admin.ModelAdmin):
+#     passs
+
+class ReviewModelAdmin(admin.ModelAdmin):
     pass
+
+class CommentsModelAdmin(admin.ModelAdmin):
+    pass
+
+class MovieInformationModelAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(ReviewModel, ReviewModelAdmin)
+admin.site.register(CommentsModel, CommentsModelAdmin)
+admin.site.register(MovieInformationModel, MovieInformationModelAdmin)
