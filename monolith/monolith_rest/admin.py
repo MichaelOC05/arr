a7650ae245961eb36a9980993cc6fff79e98cdf2
Michael O'Connell <mikematoconnell@gmail.com>
2022-08-25 18:53:56 -0400

    merge in admin

--- monolith/monolith_rest/admin.py
+++ monolith/monolith_rest/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from monolith_rest.models import ReviewModel, CommentsModel, MovieInformationModel
 # Register your models here.
-<<<<<<< HEAD
-=======
 # from .models import USER_MODEL
 
 # @admin.register(USER_MODEL)
@@ -21,4 +19,4 @@ class MovieInformationModelAdmin(admin.ModelAdmin):
 admin.site.register(ReviewModel, ReviewModelAdmin)
 admin.site.register(CommentsModel, CommentsModelAdmin)
 admin.site.register(MovieInformationModel, MovieInformationModelAdmin)
->>>>>>> 866aa77bdc0437393c718b531e9aa7f1062612e9
+
