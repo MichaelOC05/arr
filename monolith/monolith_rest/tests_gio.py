a8e90201699a08da98bdffced1dc7f26d722e6a6
Gio <giomedina321@live.com>
2022-09-06 18:09:04 -0500

    added to yml

--- monolith/monolith_rest/tests_gio.py
+++
@@ -1,6 +0,0 @@
-# from urllib import response
-# from django.test import TestCase, Client
-# from django.urls import reverse
-# from .models import MovieInformationModel
-
-# class Test_sign_up_returns_200:
