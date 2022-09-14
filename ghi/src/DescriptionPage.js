c87038159d515ccaae2008c7c6a8bb4b83baf389
Michael O'Connell <mikematoconnell@gmail.com>
2022-09-14 18:27:08 -0400

    took out universal cookies

--- ghi/src/DescriptionPage.js
+++ ghi/src/DescriptionPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import Cookies from 'universal-cookie';
 import { useToken } from "./TokenContext";
 
 function UserGreeting(props) {
