f0f51ff227e299959ea0e67ca0a421f8f256ae9f
Jack <jvfei@me.com>
2022-09-15 11:30:11 -0700

    created test to see if review exists

--- monolith/monolith_rest/tests.py
+++ monolith/monolith_rest/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from .models import MovieInformationModel, ReviewModel
+from .models import MovieInformationModel, ReviewModel, UserModel
 
 
 # Create your tests here.
@@ -11,6 +11,29 @@ class TestMovieView(TestCase):
     def test_list_movies(self):
         client = Client()
         response = client.get(reverse("list_of_movies"))
+        self.assertEquals(response.status_code, 200)
+
+#Jack Feiner's Tests
+class TestReviewModel(TestCase):
+    def setUp(self):
+        movie = MovieInformationModel.objects.create(movie_name="Spiderman")
+        UserModel.objects.create(username="Bobo", password="13579", email="bob@1234.com", first_name="Bob", last_name="Obo")
+        user = UserModel.objects.get(username="Bobo")
+        ReviewModel.objects.create(
+            movie_id=movie,
+            base_rating="9",
+            plot_rating="7",
+            char_rating="8",
+            setting_rating="10",
+            add_on_rating="9",
+            removal_rating="8",
+            rating_description="meh",
+            reviewer_id=user
+            )
+
+    def test_review_exits(self):
+        client = Client()
+        response = client.get(reverse("list_of_reviews"))
         self.assertEquals(response.status_code, 200)
 
         
